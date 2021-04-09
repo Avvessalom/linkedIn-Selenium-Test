fb345625de311591d765271dc1b176c7f44f7bc8
Avvessalom <49491863+Avvessalom@users.noreply.github.com>
2021-04-10 00:51:06 +0300

    update main page

--- src/main/kotlin/com/gmail/eugene/lazurin/linkedIn_Selenium_Test/MainPage.kt
+++ src/main/kotlin/com/gmail/eugene/lazurin/linkedIn_Selenium_Test/MainPage.kt
@@ -21,6 +21,38 @@ class MainPage(driver: WebDriver) {
     @FindBy(className = "sign-in-form__submit-button")
     lateinit var bigEnterButton: WebElement
 
+
+    @FindBy(xpath = "//a[@class='pill']")
+    lateinit var engineer: WebElement
+
+    @FindBy(xpath = "(//a[@class='pill'])[2]")
+    lateinit var business: WebElement
+
+    @FindBy(xpath = "(//a[@class='pill'])[3]")
+    lateinit var fin: WebElement
+
+    @FindBy(xpath = "(//a[@class='pill'])[4]")
+    lateinit var admin: WebElement
+
+    @FindBy(xpath = "(//a[@class='pill'])[5]")
+    lateinit var sell: WebElement
+
+    @FindBy(xpath = "(//a[@class='pill'])[6]")
+    lateinit var help: WebElement
+
+    @FindBy(xpath = "(//a[@class='pill'])[7]")
+    lateinit var operDo: WebElement
+
+    @FindBy(xpath = "(//a[@class='pill'])[8]")
+    lateinit var iT: WebElement
+
+    @FindBy(xpath = "(//a[@class='pill'])[9]")
+    lateinit var marketing: WebElement
+
+    @FindBy(xpath = "(//a[@class='pill'])[10]")
+    lateinit var cadr: WebElement
+
+
     @FindBy(className = "google-sign-in-cta__button")
     lateinit var googleEnterButton: WebElement
 
