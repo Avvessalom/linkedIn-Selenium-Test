dc6a7fad44667a1c8bc87ccfadb5ef4525763333
Avvessalom <49491863+Avvessalom@users.noreply.github.com>
2021-04-10 00:52:14 +0300

    update main page tests

--- src/test/kotlin/com/gmail/eugene/lazurin/linkedIn_Selenium_Test/MainPageTest.kt
+++ src/test/kotlin/com/gmail/eugene/lazurin/linkedIn_Selenium_Test/MainPageTest.kt
@@ -6,6 +6,8 @@ import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.interactions.Actions
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
 import java.util.concurrent.TimeUnit
 
 class MainPageTest {
@@ -34,9 +36,48 @@ class MainPageTest {
 
     @Test
     fun mainPage() {
-        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-            assertEquals("lalala", driver.title)
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
     }
 
+    @Test
+    fun enterButton() {
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+        mainPage.navButtonEnter.click()
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+    }
 
+    @Test
+    fun joinButton() {
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+        mainPage.navButtonJoin.click()
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+    }
+
+    @Test
+    fun googleButton() {
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+        mainPage.googleEnterButton.click()
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+    }
+
+    @Test
+    fun makeVacancy() {
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+        mainPage.makeVacancy.click()
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+    }
+
+    @Test
+    fun startBigButton() {
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+        mainPage.startBigButton.click()
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+    }
+
+    @Test
+    fun findGuys() {
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+        mainPage.findGuys.click()
+        WebDriverWait(driver, 5).until(ExpectedConditions.titleIs("lalala"))
+    }
 }
