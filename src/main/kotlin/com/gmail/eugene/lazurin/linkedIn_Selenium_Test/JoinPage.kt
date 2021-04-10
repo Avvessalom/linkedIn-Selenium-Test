297005a0542c7572d6d277e74981981a9af51fc2
Avvessalom <49491863+Avvessalom@users.noreply.github.com>
2021-04-10 19:32:25 +0300

    register page test

--- src/main/kotlin/com/gmail/eugene/lazurin/linkedIn_Selenium_Test/JoinPage.kt
+++ src/main/kotlin/com/gmail/eugene/lazurin/linkedIn_Selenium_Test/JoinPage.kt
@@ -1,4 +1,39 @@
 package com.gmail.eugene.lazurin.linkedIn_Selenium_Test
 
-class JoinPage {
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.support.FindBy
+import org.openqa.selenium.support.PageFactory
+
+class JoinPage(driver: WebDriver) {
+    @FindBy(id = "email-address")
+    lateinit var email: WebElement
+
+    @FindBy(id = "password")
+    lateinit var pass: WebElement
+
+    @FindBy(id = "join-form-submit")
+    lateinit var submit: WebElement
+
+    @FindBy(xpath = "//button[@class='third-party-join__btn third-party-join__google-btn']")
+    lateinit var google: WebElement
+
+    @FindBy(className = "main__sign-in-link")
+    lateinit var sign_in: WebElement
+
+    @FindBy(id = "first-name")
+    lateinit var firstName: WebElement
+
+    @FindBy(id = "last-name")
+    lateinit var lastName: WebElement
+
+    @FindBy(id = "join-form-submit")
+    lateinit var joinForm: WebElement
+
+    @FindBy(id = "register-verification-phone-number")
+    lateinit var phone: WebElement
+
+    init {
+        PageFactory.initElements(driver, this)
+    }
 }
---
+++ src/test/kotlin/com/gmail/eugene/lazurin/linkedIn_Selenium_Test/JoinPageTest.kt
@@ -0,0 +1,51 @@
+package com.gmail.eugene.lazurin.linkedIn_Selenium_Test
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import java.util.concurrent.TimeUnit
+
+class JoinPageTest {
+    private lateinit var driver: WebDriver
+    private lateinit var joinPage: JoinPage
+    private val url = "https://www.linkedin.com/signup/"
+
+    @BeforeEach
+    fun setUp() {
+        System.setProperty(
+            "webdriver.chrome.driver",
+            "src/main/kotlin/com/gmail/eugene/lazurin/linkedIn_Selenium_Test/driver/chromedriver.exe"
+        )
+        driver = ChromeDriver()
+        driver.manage().window().maximize()
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+        driver.get(url)
+
+        joinPage = JoinPage(driver)
+    }
+
+//    @AfterEach
+//    fun tearDown() {
+//        driver.quit()
+//    }
+
+    @Test
+    fun reg() {
+        joinPage.email.sendKeys(ConfProperties.getProperty("reglog"))
+        joinPage.pass.sendKeys(ConfProperties.getProperty("regpass"))
+        joinPage.submit.click()
+        joinPage.firstName.sendKeys("testname")
+        joinPage.lastName.sendKeys("testlast")
+        joinPage.joinForm.click()
+        joinPage.phone.sendKeys(ConfProperties.getProperty("gerphone"))
+    }
+
+    @Test
+    fun sign_in() {
+        joinPage.sign_in.click()
+        val loginPage = LoginPageTest()
+        loginPage.login()
+    }
+}
