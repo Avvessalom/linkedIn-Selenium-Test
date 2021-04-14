20a98577bba4b2b605a94dc8b5e4895a10031e2f
Avvessalom <49491863+Avvessalom@users.noreply.github.com>
2021-04-14 23:10:15 +0300

    update main page test, add make meeting

--- src/test/kotlin/com/gmail/eugene/lazurin/linkedIn_Selenium_Test/MainUserPageTest.kt
+++ src/test/kotlin/com/gmail/eugene/lazurin/linkedIn_Selenium_Test/MainUserPageTest.kt
@@ -1,5 +1,6 @@
 package com.gmail.eugene.lazurin.linkedIn_Selenium_Test
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -58,9 +59,11 @@ class MainUserPageTest {
         loginPage.submit.click()
         mainUserPage.nawPost.click()
         driver.findElement(By.xpath("//div[@role='textbox']")).sendKeys("Test Post 1")
-        driver.findElement(By.xpath("//button[contains(@class,'guider-button-prompt__button share-suggested-hashtags-prompt__add-hashtag')]")).click()
+        driver.findElement(By.xpath("//button[contains(@class,'guider-button-prompt__button share-suggested-hashtags-prompt__add-hashtag')]"))
+            .click()
         driver.findElement(By.xpath("//div[@role='textbox']")).sendKeys("testHashtag1")
-        val sendPost = driver.findElement(By.xpath("//div[@class='share-box_actions']//button[contains(@class,' artdeco-button')]"))
+        val sendPost =
+            driver.findElement(By.xpath("//div[@class='share-box_actions']//button[contains(@class,' artdeco-button')]"))
         val builder = Actions(driver)
         builder.moveToElement(sendPost).click(sendPost)
         builder.perform()
@@ -76,14 +79,48 @@ class MainUserPageTest {
         loginPage.pass.sendKeys(ConfProperties.getProperty("pass"))
         loginPage.submit.click()
         mainUserPage.vacancy.click()
-        driver.findElement(By.xpath("//input[@class='jobs-search-box__text-input jobs-search-box__keyboard-text-input']")).sendKeys("developer")
-        val citySearch = driver.findElement(By.xpath("//input[@class='jobs-search-box__text-input jobs-search-box__ghost-text-input']"))
+        driver.findElement(By.xpath("//input[@class='jobs-search-box__text-input jobs-search-box__keyboard-text-input']"))
+            .sendKeys("developer")
+        val citySearch =
+            driver.findElement(By.xpath("//input[@class='jobs-search-box__text-input jobs-search-box__ghost-text-input']"))
         val builder = Actions(driver)
         builder.moveToElement(citySearch).click(citySearch)
         builder.sendKeys("St Petersburg" + Keys.ENTER)
         builder.perform()
-        val post = driver.findElements(By.xpath("//li[@class='jobs-search-results__list-item occludable-update p0 relative ember-view']"))[0].text
+        val post =
+            driver.findElements(By.xpath("//li[@class='jobs-search-results__list-item occludable-update p0 relative ember-view']"))[0].text
         print(post)
         assertTrue(post.contains("developer") || post.contains("Developer"))
+    }
+
+    @Test
+    fun makeMeeting() {
+        loginPage.username.sendKeys(ConfProperties.getProperty("login"))
+        loginPage.pass.sendKeys(ConfProperties.getProperty("pass"))
+        loginPage.submit.click()
+        mainUserPage.wide.click()
+        driver.findElement(By.xpath("//a[@data-control-name='events']")).click()
+        driver.findElement(By.xpath("//button[@data-control-name='curation_hub_create_event']")).click()
+        driver.findElement(By.id("events-shared-creation-form__name")).sendKeys("testMeeting")
+        driver.findElement(By.xpath("//div[contains(@class,'full-width t-14')]")).click()
+        driver.findElement(By.xpath("//div[text()[normalize-space()='(UTC-02:00) Гренландия']]")).click()
+        driver.findElement(By.id("date-time-picker__start-date")).sendKeys(Keys.BACK_SPACE)
+        driver.findElement(By.id("date-time-picker__start-date")).sendKeys("2")
+        driver.findElement(By.id("date-time-picker__end-date")).sendKeys(Keys.BACK_SPACE)
+        driver.findElement(By.id("date-time-picker__end-date")).sendKeys("2")
+        driver.findElement(By.id("date-time-picker__start-time")).sendKeys("1234")
+        driver.findElement(By.id("date-time-picker__end-time")).sendKeys("1345")
+        driver.findElement(By.id("events-shared-details-section__description")).sendKeys("test meeting text area")
+        driver.findElement(By.xpath("(//div[contains(@class,'full-width t-14')])[2]")).click()
+        driver.findElement(By.xpath("//div[contains(@class,'events-shared-event-visibility__dropdown-option artdeco-dropdown__item')]"))
+            .click()
+        driver.findElement(By.xpath("(//span[@class='artdeco-button__text'])[2]")).click()
+        driver.findElement(By.xpath("//a[@data-control-name='event_share_card_cta']")).click()
+        val testMeeting = driver.findElement(By.xpath("//div[@class='events-top-card__info-container']//h1"))
+            .getAttribute("innerHTML")
+        assertEquals(
+            testMeeting, "\n      testMeeting\n    "
+        )
+        println(testMeeting)
     }
 }
